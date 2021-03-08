d8e933de974970f173f510f5fe963931179a48f5
Diogo Oliveira <diogo.oliveira@my.istec.pt>
2021-03-08 10:42:11 +0100

    Implement all CRUD

--- CinemaApi/Controllers/MoviesController.cs
+++ CinemaApi/Controllers/MoviesController.cs
@@ -31,27 +31,37 @@ namespace CinemaApi.Controllers
 
         // GET api/<MoviesController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Movie Get(int id)
         {
-            return "value";
+            var movie = _dbContext.Movies.Find(id);
+            return movie;
         }
 
         // POST api/<MoviesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Movie movie)
         {
+            _dbContext.Movies.Add(movie);
+            _dbContext.SaveChanges();
         }
 
         // PUT api/<MoviesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Movie movie)
         {
+            var movieDb = _dbContext.Movies.Find(id);
+            movieDb.Name = movie.Name;
+            movieDb.Language = movie.Language;
+            _dbContext.SaveChanges();
         }
 
         // DELETE api/<MoviesController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var movieDb = _dbContext.Movies.Find(id);
+            _dbContext.Movies.Remove(movieDb);
+            _dbContext.SaveChanges();
         }
     }
 }
