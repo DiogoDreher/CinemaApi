69b595b54b25401a1ebd359dbc3927e448e0d836
Diogo Oliveira <diogo.oliveira@my.istec.pt>
2021-03-08 16:12:18 +0100

    Implement Authentication and Authorization with JWT

--- CinemaApi/Startup.cs
+++ CinemaApi/Startup.cs
@@ -13,6 +13,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 
 namespace CinemaApi
 {
@@ -34,6 +37,22 @@ namespace CinemaApi
             services.AddDbContext<CinemaDbContext>(option => option.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; 
                                                                                    Initial Catalog = CinemaDb; 
                                                                                    Integrated Security = True"));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Tokens:Issuer"],
+                        ValidAudience = Configuration["Tokens:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"])),
+                        ClockSkew = TimeSpan.Zero,
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +68,8 @@ namespace CinemaApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
