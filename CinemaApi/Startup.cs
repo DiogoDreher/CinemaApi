020865c57bb9514206b948e6d831ab6b302e1807
Diogo Oliveira <diogo.oliveira@my.istec.pt>
2021-03-08 10:31:34 +0100

    Add SQL Server Db Connection

--- CinemaApi/Startup.cs
+++ CinemaApi/Startup.cs
@@ -1,7 +1,9 @@
+using CinemaApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,10 +30,13 @@ namespace CinemaApi
         {
 
             services.AddControllers();
+            services.AddDbContext<CinemaDbContext>(option => option.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; 
+                                                                                   Initial Catalog = CinemaDb; 
+                                                                                   Integrated Security = True"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CinemaDbContext dbContext)
         {
             if (env.IsDevelopment())
             {
@@ -43,6 +48,8 @@ namespace CinemaApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            dbContext.Database.EnsureCreated();
 
             app.UseEndpoints(endpoints =>
             {
