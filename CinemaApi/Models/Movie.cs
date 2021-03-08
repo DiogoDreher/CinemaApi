8fc14f1977b52651c4f5ac413b2eab8f59691259
Diogo Oliveira <diogo.oliveira@my.istec.pt>
2021-03-08 12:02:41 +0100

    Add Model Validation with Data Annotations

--- CinemaApi/Models/Movie.cs
+++ CinemaApi/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +11,14 @@ namespace CinemaApi.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name cannot be null or empty")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Language cannot be null or empty")]
         public string Language { get; set; }
+
+        [Required]
         public double Rating { get; set; }
 
         [NotMapped]
