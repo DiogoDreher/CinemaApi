d779714db2c8d7cd17323fd9761d5aaa38c77ab4
Diogo Oliveira <diogo.oliveira@my.istec.pt>
2021-03-08 15:20:53 +0100

    Create new necessary models and implement database relationships

--- CinemaApi/Models/Movie.cs
+++ CinemaApi/Models/Movie.cs
@@ -14,15 +14,26 @@ namespace CinemaApi.Models
 
         [Required(ErrorMessage = "Name cannot be null or empty")]
         public string Name { get; set; }
-
+        [Required]
+        public string Description { get; set; }
         [Required(ErrorMessage = "Language cannot be null or empty")]
         public string Language { get; set; }
-
         [Required]
+        public string Duration { get; set; }
+        [Required]
+        public DateTime PlayingDate { get; set; }
+        [Required]
+        public DateTime PlayingTime { get; set; }
+        [Required]
+        public double TicketPrice { get; set; }
         public double Rating { get; set; }
-
+        [Required]
+        public string Genre { get; set; }
+        public string TrailerUrl { get; set; }
+        public string ImageUrl { get; set; }
         [NotMapped]
         public IFormFile Image { get; set; }
-        public string ImageUrl { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; }
     }
 }
